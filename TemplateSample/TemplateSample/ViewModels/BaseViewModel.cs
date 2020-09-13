c74a6f00bd776c899665938ec8c71024e88ec054
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-09-13 11:05:30 -0300

    Add BaseViewModel

---
+++ TemplateSample/TemplateSample/ViewModels/BaseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+
+namespace TemplateSample.ViewModels
+{
+    public class BaseViewModel : BindableBase, INavigationAware, IInitialize
+    {
+        protected readonly INavigationService _navigationService;
+
+        public BaseViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+        public virtual void Initialize(INavigationParameters parameters)
+        {
+            
+        }
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+            
+        }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+            
+        }
+    }
+}
