67b5a1255ee79058470058ce6689569a51966c06
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-09-14 11:52:30 -0300

    Add talk list

---
+++ TemplateSample/TemplateSample/Models/Talk.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TemplateSample.Models
+{
+    public class Talk
+    {
+        public string Title { get; set; }
+        public string Speaker { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- TemplateSample/TemplateSample/ViewModels/MainViewModel.cs
+++ TemplateSample/TemplateSample/ViewModels/MainViewModel.cs
@@ -1,13 +1,37 @@
-﻿using Prism.Navigation;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Prism.Navigation;
+using TemplateSample.Models;
 
 namespace TemplateSample.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        public ObservableCollection<Talk> Talks { get; private set; }
+
         public MainViewModel(INavigationService navigationService):
             base(navigationService)
         {
-            
+            Talks = new ObservableCollection<Talk>();
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if (Talks.Any())
+                return;
+
+            for (int i = 1; i <= 20; i++)
+            {
+                Talks.Add(new Talk
+                {
+                    Title = $"Talk title {i}",
+                    Speaker = $"Speaker {i}",
+                    Date = DateTime.Now.AddDays(i)
+                });
+            }
         }
     }
 }
